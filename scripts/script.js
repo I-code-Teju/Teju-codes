b677afa35bf8d42f30a6f96d6366d18b62011b00
Tejuosho David <124820049+I-code-Teju@users.noreply.github.com>
2023-08-30 14:26:59 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
 
+const toggler = document.querySelector(‘.toggleBtn’)
+toggler.addEventListener(‘click’, ()=>{
+ const dropDown= document.querySelector(‘.dropDown’)
+dropDown.classList.toggle(‘show’)
+})
